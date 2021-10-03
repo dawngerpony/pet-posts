2885ea8da35664becfbcb40932ec653fa92bbc11
Dawn James <dawnjames2105@gmail.com>
2021-10-03 12:20:04 +0100

    Move image list to a separate file.

--- pet_posts/images.py
+++ pet_posts/images.py
@@ -2,11 +2,6 @@ import random
 
 
 def get_image_url():
-    urls = [
-        "https://www.southamerica.travel/wp-content/uploads/2018/02/10-amazing-facts-about-Capybara-1.jpg",
-        "https://www.zastavki.com/pictures/originals/2015/Animals___Rodents_____Hamster_on_a_rope_092822_.jpg",
-        "https://s-i.huffpost.com/gen/2034584/images/o-PUDDING-THE-FOX-facebook.jpg",
-        "https://www.zooeasy.com/wp-content/uploads/2019/07/jack-catalano-q1pw1dOW0_0-unsplash.jpg",
-        "https://i.huffpost.com/gen/911965/images/o-CUTEST-BABY-ANIMAL-facebook.jpg",
-    ]
-    return random.choice(urls)
+    with open("image_urls.txt") as f:
+        image_urls = f.readlines()
+    return random.choice(image_urls)
--- tests/test_images.py
+++ tests/test_images.py
@@ -1,6 +1,10 @@
+from unittest.mock import patch
 from pet_posts.images import get_image_url
 
 
-def test_get_image_url():
-    image_url = get_image_url()
-    assert image_url.startswith("http")
+def test_get_image_url(mocker):
+    url = "https://www.example.com/image.jpg"
+    mock_open = mocker.mock_open(read_data=url)
+    with patch('pet_posts.images.open', mock_open):
+        image_url = get_image_url()
+    assert image_url == url
